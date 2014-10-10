0a7a568e88249890fcfd00a7b84e7cf3c3e25e4a
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2014-10-10 15:29:25 +0200

    working cap file

--- deploy.rb
+++ deploy.rb
@@ -1,67 +1,73 @@
-set :application, 'spree'
-set :repo_url, 'git://github.com/radar/nf_performance_spree'
-set :deploy_to, '/data/spree'
-set :log_level, :debug
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{tmp/sockets log config/puma public/spree}
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'tweetstream'
+set :repo_url, 'https://github.com/rocky-jaiswal/tweetstream.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/opt/tweetstream'
 set :sockets_path, Pathname.new("#{fetch(:deploy_to)}/shared/tmp/sockets/")
+set :puma_state, fetch(:sockets_path).join('puma.state')
 
-# These puma settings are only here because capistrano-puma is borked.
-# See issue #4.
-set :puma_roles, :app
-set :puma_socket, "unix://#{fetch(:sockets_path).join('puma_' + fetch(:application) + '.sock')}"
-set :pumactl_socket, "unix://#{fetch(:sockets_path).join('pumactl_' + fetch(:application) + '.sock')}"
-set :puma_state, fetch(:sockets_path).join('puma.state') 
-set :puma_log, -> { shared_path.join("log/puma-#{fetch(:stage )}.log") }
-set :puma_flags, nil 
+# Default value for :scm is :git
+# set :scm, :git
 
-set :bundle_flags, '--deployment'
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
 
 namespace :deploy do
-  task :restart do
-    invoke 'puma:restart'
-  end
-end
 
-namespace :spree_sample do
-  task :load do
-    on roles(:app) do
+  desc 'Symlink shared configs and folders on each release.'
+  task :symlink_shared do
+    on roles(:app), in: :sequence do
       within release_path do
-        ask(:confirm, "Are you sure you want to delete everything and start again? Type 'yes'")
-        if fetch(:confirm) == "yes"
-          execute :rake, "db:reset AUTO_ACCEPT=true"
-          execute :rake, "spree_sample:load"
-        end
+        execute :ln , "-nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
       end
     end
   end
-end
 
-namespace :puma do
-  desc "Restart puma instance for this application"
-  task :restart do
-    on roles fetch(:puma_roles) do
+  desc 'build grunt app'
+  task :grunt_build do
+    on roles(:web), in: :sequence do
+      within "#{release_path}/public/angular-app" do
+        execute :npm, "install"
+        execute :bower, "install"
+        execute :grunt, "build"
+      end
+    end
+  end
+
+  desc 'restart puma'
+  task :puma_restart do
+    on roles(:app), in: :sequence do
       within release_path do
         execute :bundle, "exec pumactl -S #{fetch(:puma_state)} restart"
       end
     end
   end
 
-  desc "Show status of puma for this application"
-  task :status do
-    on roles fetch(:puma_roles) do
-      within release_path do
-        execute :bundle, "exec pumactl -S #{fetch(:puma_state)} stats"
-      end
-    end
-  end
-
-  desc "Show status of puma for all applications"
-  task :overview do
-    on roles fetch(:puma_roles) do
-      within release_path do
-        execute :bundle, "exec puma status"
-      end
-    end
-  end
+  before :migrate, :symlink_shared
+  after  :published, :grunt_build
+  after  :finished, :puma_restart
 end
